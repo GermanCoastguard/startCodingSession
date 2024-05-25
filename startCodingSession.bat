45517e2c6ad272cffa33ef17b500358adf9faab3
Marcel_Z <113604651+GermanCoastguard@users.noreply.github.com>
2024-05-26 00:02:30 +0200

    Update startCodingSession.bat

--- startCodingSession.bat
+++ startCodingSession.bat
@@ -1,9 +1,51 @@
-
 echo off
 
 go to end
 
 :end
-start "" "your path ... \colorPicker.ahk"
-start "" "your path ... \lightshotLive.ahk"
-start "" "your path ... \switchVscWindow.ahk"
+start "" "YOUR PATH \colorPicker.ahk"
+start "" "YOUR PATH \lightshotLive.ahk"
+start "" "YOUR PATH \switchVscWindow.ahk"
+
+start chrome.exe
+start chrome.exe "https://github.com/"
+timeout /t 0.1
+start "" "YOUR PATH \ctrl_1.ahk"
+timeout /t 0.005
+start "" "YOUR PATH \ctrl_w.ahk"
+timeout /t 0.005
+start "" "YOUR PATH \send_win_left.ahk"
+timeout /t 0.0001
+start "" "YOUR PATH \send_win_left.ahk"
+timeout /t 0.0001
+start "" "YOUR PATH \send_win_left.ahk"
+timeout /t 0.0001
+start "" "YOUR PATH \send_win_left.ahk"
+timeout /t 0.0001
+start "" "YOUR PATH \send_win_left.ahk"
+timeout /t 0.0001
+
+
+start chrome.exe
+start chrome.exe "https://chat.openai.com/chat"
+timeout /t 0.1
+start "" "YOUR PATH \ctrl_1.ahk"
+timeout /t 0.005
+start "" "YOUR PATH \ctrl_w.ahk"
+start "" "YOUR PATH \send_win_left.ahk"
+timeout /t 0.0001
+start "" "YOUR PATH \send_win_left.ahk"
+timeout /t 0.0001
+
+start chrome.exe
+start chrome.exe "https://www.google.com"
+timeout /t 0.1
+start "" "YOUR PATH \ctrl_1.ahk"
+timeout /t 0.005
+start "" "YOUR PATH \ctrl_w.ahk"
+start "" "YOUR PATH \send_win_left.ahk"
+timeout /t 0.0001
+
+
+exit
+github
