d8454772c9c02b1fd41305924a359b8f784b7b4d
Marcel_Z <113604651+GermanCoastguard@users.noreply.github.com>
2024-05-26 16:06:00 +0200

    Update startCodingSession.bat

--- startCodingSession.bat
+++ startCodingSession.bat
@@ -1,4 +1,4 @@
-echo off
+@echo off
 
 start "" /b "YOUR PATH\colorPicker.ahk"
 start "" /b "YOUR PATH\lightshotLive.ahk"
@@ -8,7 +8,9 @@ start "" /b "YOUR PATH\wrapTextIntoInterpolation.ahk"
 start "" /b "YOUR PATH\wrapTextIntoStringInterpolation.ahk"
 
 start chrome.exe "https://github.com/"
-timeout /t 1
+timeout /t 2
+start "" /b "YOUR PATH\send_win_left.ahk"
+timeout /t 0.2
 start "" /b "YOUR PATH\send_win_left.ahk"
 timeout /t 0.2
 start "" /b "YOUR PATH\send_win_left.ahk"
@@ -23,7 +25,9 @@ timeout /t 1
 start "" /b "YOUR PATH\ctrl_n.ahk"
 timeout /t 1
 start "" /b "YOUR PATH\openAiUrl.ahk"
-timeout /t 1.5
+timeout /t 2
+start "" /b "YOUR PATH\send_win_left.ahk"
+timeout /t 0.1
 start "" /b "YOUR PATH\send_win_left.ahk"
 timeout /t 0.1
 start "" /b "YOUR PATH\send_win_left.ahk"
@@ -33,10 +37,16 @@ timeout /t 1
 start "" /b "YOUR PATH\ctrl_n.ahk"
 timeout /t 1
 start "" /b "YOUR PATH\googleUrl.ahk"
-timeout /t 1.5
+timeout /t 2
 start "" /b "YOUR PATH\send_win_left.ahk"
+timeout /t 0.1
+start "" /b "YOUR PATH\send_win_left.ahk"
+timeout /t 0.1
+start "" /b "YOUR PATH\openChromeDevTools.ahk"
 timeout /t 1
 
-
+start "" /b "YOUR PATH\cB.xlsx
+timeout /t 1
+start "" /b "YOUR PATH\send_win_left.ahk"
 
 exit
