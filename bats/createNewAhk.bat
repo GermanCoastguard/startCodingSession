288f21acd2106627a6bd4c0d92055e9ba822e4ef
Marcel_Z <113604651+GermanCoastguard@users.noreply.github.com>
2024-05-28 01:43:18 +0200

    Create createNewAhk.bat

---
+++ bats/createNewAhk.bat
@@ -0,0 +1,7 @@
+echo off
+REM Ref Datei  v     v     v     v   v
+REM createNewVscTabWithAhkAndBatTemplate 
+REM Ref Datei  ^     ^     ^     ^   ^
+
+start "" /b "YOUR PATH \createNewVscTabWithAhkAndBatTemplate.ahk"
+exit
